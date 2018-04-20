12fec5edb2834a8d1a84c15ca468793cc7f8865e
Joosep Alviste <joosep.alviste@gmail.com>
2018-04-20 23:32:19 +0200

    Set up Babel

--- src/index.js
+++ src/index.js
@@ -9,3 +9,7 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+const arr = [1, 2, 3];
+const iAmJavascriptES6 = () => console.log(...arr);
+window.iAmJavascriptES6 = iAmJavascriptES6;
