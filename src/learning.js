0bb58e62cd8fd1ebf6ef3c74117f7b2de6f58516
k <krajle@gmail.com>
2018-05-02 11:13:56 +0200

    random initial angle

--- src/learning.js
+++ src/learning.js
@@ -2,41 +2,41 @@ import { Breakout, BreakoutSettings } from './breakout'
 import {canvas, ctx, resizeCanvas} from './core/utils/canvas';
 
 function startProgrammaticControlledGame() {
-  const game = new Breakout({
-    settings: new BreakoutSettings({
-      width: 28,
-      height: 28,
+    const game = new Breakout({
+        settings: new BreakoutSettings({
+            width: 28,
+            height: 28,
+            
+            brickHeight: 1,
+            brickColumnCount: 3,
+            brickRowCount: 2,
 
-      brickHeight: 1,
-      brickColumnCount: 3,
-      brickRowCount: 2,
+            paddleHeight: 1,
+            paddleWidth: 4,
+            paddleVelocity: 200,
 
-      paddleHeight: 1,
-      paddleWidth: 4,
-      paddleVelocity: 200,
+            ballRadius: 0.5,
+            ballVelocity: 150,
+            ballInitialAngle: Math.random()*(2.35-0.785) + 0.785
+        }),
+    });
 
-      ballRadius: 0.5,
-      ballVelocity: 150,
-    }),
-  });
+    game.reset();
 
-  game.reset();
-
-  return game;
+    return game;
 }
 
 async function sleep(time) {
-  return new Promise(resolve => setTimeout(resolve, time));
+    return new Promise(resolve => setTimeout(resolve, time));
 }
 
 export const raw = ctx.getImageData(0,0,canvas.width,canvas.height);
+const g = startProgrammaticControlledGame();
+window.g = g;
 
 export async function loop(n) {
-  const g = startProgrammaticControlledGame();
-  window.g = g;
-
-  for (let i = 0; i < n; i++) {
-    await sleep(10);
-    g.step(Math.floor(Math.random() * 3));
-  }
+    for (let i = 0; i < n; i++) {
+        await sleep(10);
+        g.step(Math.floor(Math.random() * 3));
+    }
 }
